0c6c36174596a22ec24002b809457144c0c03ed2
Kim Bum Jun <admin@vivabj.com>
2023-12-10 23:05:37 +0900

    Tetris Game [new]

---
+++ WinFormNConsoleCamp/VivAdv/Tetris/OBlock.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Tetris
+{
+    public class OBlock : Block
+    {
+        private readonly Position[][] tiles = [
+            [new(0, 0), new(0, 1), new(1, 0), new(1, 1)]
+        ];
+        protected override Position[][] Tiles => tiles;
+
+        protected override Position StartOffset => new(0, 4);
+
+        public override int Id => 4;
+    }
+}
