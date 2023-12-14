651ef1169287433e3e53e901eeb6aa3d60926487
ViVaKR <admin@vivabj.com>
2023-12-14 20:30:47 +0900

    Add Solution.Forms

---
+++ WinFormNConsoleCamp/Solution.Forms/WpfDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfDataBinding
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [AllowNull]
+        private string boundText;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public MainWindow()
+        {
+            // 점 3개 없에기
+            DataContext = this;
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void BtnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void BtnMessage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your Message Here.", "");
+        }
+    }
+}
---
+++ WinFormNConsoleCamp/Solution.Forms/WpfTutorial/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorial.View.UserControls
+{
+    public partial class ClearableTextBox : UserControl
+    {
+        [AllowNull]
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value;
+
+                // Do not do this!
+                TxblPlaceholder.Text = placeholder;
+                // OnPropertyChanged()
+            }
+        }
+
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxbInput.Clear();
+            TxbInput.Focus();
+        }
+
+        private void TxbInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is not TextBox textbox) return;
+
+            TxblPlaceholder.Visibility = string.IsNullOrEmpty(textbox.Text) ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+}
