59921303e417a300bf6ad54a71772201fe12d5fa
Kim Bum Jun <admin@vivabj.com>
2023-12-15 16:04:41 +0900

    WpfListView [add]

---
+++ WinFormNConsoleCamp/Solution.Forms/WpfListView/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Windows;
+
+namespace WpfListView
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            LsvEntries.Items.Add("a");
+            LsvEntries.Items.Add("b");
+            LsvEntries.Items.Add("c");
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            LsvEntries.Items.Add(TbEntry.Text);
+            TbEntry.Clear();
+            TbEntry.Focus();
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            // by index
+            //int idx = LsvEntries.SelectedIndex;
+            //LsvEntries.Items.RemoveAt(idx);
+
+            // by item
+            object item = LsvEntries.SelectedItem;
+
+            var confirm = MessageBox.Show($"Are you sure you want to delete: `{item}`", "Confirm", MessageBoxButton.YesNo);
+            if (confirm == MessageBoxResult.Yes)
+                LsvEntries.Items.Remove(item);
+
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            LsvEntries.Items.Clear();
+        }
+    }
+}
