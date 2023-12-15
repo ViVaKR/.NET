83eb773149bf2f10781358b513c2546c002324ee
ViVaKR <admin@vivabj.com>
2023-12-15 17:32:39 +0900

    add WpfObsv

---
+++ WinFormNConsoleCamp/Solution.Forms/WpfObsv/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+
+
+namespace WpfObsv
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> entries;
+
+        public ObservableCollection<string> Entries
+        {
+            get { return entries; }
+            set
+            {
+                entries = value;
+
+            }
+        }
+
+        public MainWindow()
+        {
+            
+            DataContext = this;
+            
+            InitializeComponent();
+            entries = [];
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Add(TbEntry.Text);
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedItem = (string)LsvEntries.SelectedItem;
+            Entries.Remove(selectedItem);
+        }
+    }
+}
