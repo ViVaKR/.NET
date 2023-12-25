2a272df1c17520cc762b2237254eda99e0dd235d
Kim Bum Jun <admin@vivabj.com>
2023-12-25 14:54:23 +0900

    modified directories

--- WinFormNConsoleCamp/Solution.Consoles/BootCamp/Camp.DependencyInjection/Worker.cs
+++ WinFormNConsoleCamp/Solution.Consoles/BootCamp/Camp.DependencyInjection/Worker.cs
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 ﻿
-using Microsoft.Extensions.Hosting;
-
-=======
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
->>>>>>> f9cb5cf873c4c1c8af334968e7f1853ade154855
+
 namespace Camp.DependencyInjection;
 
 public class Worker(ILogger<Worker> logger) : BackgroundService
