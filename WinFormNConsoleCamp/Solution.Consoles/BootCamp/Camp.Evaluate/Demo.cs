9c7e4e0820c2d08f0a77dcef03fa04020cdf844d
Kim Bum Jun <admin@vivabj.com>
2024-01-03 12:39:05 +0900

    New Camp.Start

---
+++ WinFormNConsoleCamp/Solution.Consoles/BootCamp/Camp.Evaluate/Demo.cs
@@ -0,0 +1,8 @@
+﻿namespace CampEvaluate;
+
+public class Demo
+{
+    public static void Run() {
+        Console.WriteLine("Demo Run");  
+    }
+}
---
+++ WinFormNConsoleCamp/Solution.Consoles/BootCamp/Camp.Evaluate/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using CampEvaluate;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+//# (1) 프로젝트 생성 #//
+//! $ dotnet new console -o CampEvaluate
+
+//# (2) 테스트 클래서 Runner 생성 #//
+//! $ dotnet new class -n Runner
+
+//# (3) 스크립트 처리 nuget package 설치 #//
+//! $ dotnet add package Microsoft.CodeAnalysis.CSharp.Scripting
+
+//# 코드 작성 후.. //
+
+//# 실행 테스트,  수식을 제 1 Argument 로 넣기, 솔루션 에서 프로젝트 실행 할시 --project 옵션 넣기 //
+// $ dotnet run "System.Math.Sqrt(50 + 350)" --project Camp.Evaluate/CampEvaluate.csproj
+
+//==> Start Point <==//
+
+//! Example (1)
+var value = await CSharpScript.EvaluateAsync(args[0]);
+Console.WriteLine();
+Console.WriteLine($"Args[0] Expresion: {args[0]} => {value}");
+Console.WriteLine();
+
+//! Example (2)
+Runner.ExecuteScript(1200, 58);
+Console.WriteLine("====");
+Console.WriteLine();
+Runner.GetScript(40, 30);
+
+// End //
