c2d1dc9c97bb818cc052d2cafb9ff805dd946d22
Kim Bum Jun <admin@vivabj.com>
2024-02-28 16:29:49 +0900

    Create new winform project for QnA ApiRef

---
+++ WinFormNConsoleCamp/Solution.Forms/ApiRef/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApiRef
+{
+    public partial class MainForm : Form
+    {
+
+        private Panel panel;
+        private MyCalendar calendar;
+
+        public MainForm()
+        {
+
+            InitializeComponent();
+            AutoScaleMode = AutoScaleMode.Font;
+
+            WindowState = FormWindowState.Maximized;
+            StartPosition = FormStartPosition.CenterScreen;
+
+            Load += (s, e) =>
+            {
+                Controls.Add(calendar = new MyCalendar());
+                calendar.Left = (Width - calendar.Width) / 2;
+            };
+
+        }
+    }
+}
---
+++ WinFormNConsoleCamp/Solution.Forms/ApiRef/MyCalendar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApiRef
+{
+    public class MyCalendar : MonthCalendar
+    {
+        public MyCalendar()
+        {
+
+            var current = DateTime.Now;
+
+            // Font Size 로 카렌더 크기 재 조정됨
+            Font = new Font(Font.FontFamily, 48f, FontStyle.Regular);
+            MinDate = new DateTime(current.Year, current.Month, 1);
+            MaxDate = new DateTime(current.Year, current.Month, DateTime.DaysInMonth(current.Year, current.Month) - 1);
+
+        }
+
+
+
+        #region API uxtheme
+        [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        static extern int SetWindowTheme(IntPtr hwnd, string pszSubAppName, string pszSubIdList);
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            SetWindowTheme(Handle, string.Empty, string.Empty);
+            Top = 50;
+            base.OnHandleCreated(e);
+        }
+        #endregion
+    }
+}
