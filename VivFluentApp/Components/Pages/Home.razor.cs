f64c914417f7c93b1783b1ddabb24bb7ee3a4427
Kim Bum Jun <admin@vivabj.com>
2025-01-15 23:00:32 +0900

    Create new VivFluentApp

---
+++ VivFluentApp/Components/Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace VivFluentApp.Components.Pages;
+
+public partial class Home
+{
+    private string title;
+    protected override void OnInitialized()
+    {
+        title = "Home";
+    }
+}
