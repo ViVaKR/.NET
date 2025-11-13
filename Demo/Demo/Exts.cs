ac8296aa89fb5c8fa94a960705d6102438070593
Kim Bum Jun <admin@vivabj.com>
2025-11-13 09:37:06 +0900

    Create new Demo

---
+++ Demo/Demo/Exts.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo;
+
+public static class Exts
+{
+    // Extension block
+    extension<T>(IEnumerable<T> source)
+    {
+        // Extension property
+        public bool IsEmpty => !source.IsEmpty;
+    }
+}
---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+int[] numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+
+var query = from num in numbers
+            where (num % 2) == 0
+            select num;
+
+
+var str = string.Join(", ", query);
+
+Console.WriteLine($"{str}");
